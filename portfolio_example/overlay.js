dd03bd972e0291412e2627fc816a84d59e61fcae
s6thhoan <s6thhoan@uni-trier.de>
2025-08-14 13:10:13 +0200

    delete folder porfolio example

--- portfolio_example/overlay.js
+++
@@ -1,9 +0,0 @@
-var ovElemId;
-function ov_on(elem_id) {
-  ovElemId = elem_id;  
-  document.getElementById(ovElemId).style.display = "block";
-}
-
-function ov_off() {
-  document.getElementById(ovElemId).style.display = "none";
-}
--- portfolio_example/w3menu.js
+++
@@ -1,8 +0,0 @@
-function w3menu(elem_id) {
-  var x = document.getElementById(elem_id);
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
